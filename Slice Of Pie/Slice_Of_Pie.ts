7ac573327f2aca1802e821c0f7d95b563a0ab6bf
TimotejkoPantak <72004230+TimotejkoPantak@users.noreply.github.com>
2021-10-11 14:31:35 +0200

    Slice Of Pie

---
+++ Slice Of Pie/Slice_Of_Pie.ts
@@ -0,0 +1,11 @@
+var numberOfSlices = 42;
+var numberOfRecipients = 21;
+var slicesEach = 2;
+
+if((numberOfRecipients*slicesEach)>numberOfSlices){
+    console.log("FALSE");
+    
+}
+else{
+    console.log("TRUE");
+}
