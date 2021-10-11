18a93625b7bd229cff0638bb19c967c7310cf57b
TimotejkoPantak <72004230+TimotejkoPantak@users.noreply.github.com>
2021-10-11 15:33:11 +0200

    Add files via upload

---
+++ Capture the Rook/Capture_The_Rook.ts
@@ -0,0 +1,7 @@
+function canCapture(str1, str2){
+    if(str1[0] == str2[0] || str1[1] == str2[1])
+        console.log("true");
+    else
+        console.log("false");
+}
+console.log(canCapture("E5", "J5"));
