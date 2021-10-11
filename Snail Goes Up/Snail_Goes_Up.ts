7074393809c6d18470667263b5b26559e8c9a7c8
TimotejkoPantak <72004230+TimotejkoPantak@users.noreply.github.com>
2021-10-11 12:30:51 +0200

    Snail Goes Up exercise

---
+++ Snail Goes Up/Snail_Goes_Up.ts
@@ -0,0 +1,6 @@
+var heightOfStep = 0.3;
+var lenghtOfStep = 0.2;
+var heightOfTower = 25;
+
+var totalDistance = (heightOfTower/heightOfStep)*(heightOfStep+lenghtOfStep);
+console.log(totalDistance);
