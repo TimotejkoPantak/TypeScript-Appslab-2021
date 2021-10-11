88fc31e2b056a700464be1cfd61d828e411ada7c
TimotejkoPantak <72004230+TimotejkoPantak@users.noreply.github.com>
2021-10-11 15:34:05 +0200

    Capture_The_Rook

--- Capture the Rook/Capture_The_Rook.ts
+++ Capture the Rook/Capture_The_Rook.ts
@@ -2,6 +2,6 @@ function canCapture(str1, str2){
     if(str1[0] == str2[0] || str1[1] == str2[1])
         console.log("true");
     else
-        console.log("false");
+        console.log(" false");
 }
 console.log(canCapture("E5", "J5"));
